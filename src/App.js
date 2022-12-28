6f033518fb977f98127770222e74c6f63fda0c7a
phanlong99 <phanlong99mq@gmail.com>
2022-12-28 23:17:18 +0700

    TodoList Simple

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import Colorbox from "./components/box/Colorbox";
 import "./App.css";
+import TodoFeatures from "./features/Todo";
 
 function App() {
-  return (
-    <section>
-      <Colorbox color='red' height='100px' />
-      <Colorbox color='blue' height='200px' />
-      <Colorbox color='green' height='300px' />
-    </section>
-  );
+  return <TodoFeatures />;
 }
 
 export default App;
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import TodoList from "./components/TodoList";
 
 TodoFeatures.propTypes = {};
 
 function TodoFeatures(props) {
-  return <div>TodoFeatures</div>;
+  const todoList = [
+    {
+      id: "1",
+      title: "Eat",
+    },
+    {
+      id: "2",
+      title: "Code",
+    },
+    {
+      id: "3",
+      title: "Sleep",
+    },
+  ];
+  return (
+    <div>
+      Todo Features
+      <TodoList todoList={todoList} />
+    </div>
+  );
 }
 
 export default TodoFeatures;
