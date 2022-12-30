d7facb63b724bda5e103035e387d6881ecb491af
phanlong99 <phanlong99mq@gmail.com>
2022-12-31 00:32:27 +0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link, NavLink, Route, Routes, Redirect } from "react-router-dom";
 import "./App.css";
+import AlbumFeature from "./features/Album";
 import TodoFeatures from "./features/Todo";
 
 function App() {
-  return <TodoFeatures />;
+  return (
+    <div className='App'>
+      header
+      <p>
+        <NavLink to='/todo'>Todo</NavLink>
+      </p>
+      <p>
+        <NavLink to='/album'>Album</NavLink>
+      </p>
+      <Routes>
+        <Route path='/todo' element={<TodoFeatures />} />
+        <Route path='/album' element={<AlbumFeature />} />
+      </Routes>
+      footer
+    </div>
+  );
 }
 
 export default App;
